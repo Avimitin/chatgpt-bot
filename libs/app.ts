a614acd79f7329c5d715a857f7140a64591638b7
Avimitin <avimitin@gmail.com>
2023-03-06 11:55:35 +0800

    Move all the env getter into App initializer
    Signed-off-by: Avimitin <dev@avimit.in>

--- libs/app.ts
+++ libs/app.ts
@@ -1,28 +1,45 @@
-import { OpenAIApi, Configuration } from "npm:openai@3.2.1";
-import { dispatch } from "./bot.ts";
-
-export interface AppConfig {
-  bot_token: string,
-  openai_token: string,
-  whitelist: number[],
-  model: string,
-}
+import { Configuration, OpenAIApi } from "npm:openai@3.2.1";
+import * as Bot from "./bot.ts";
 
 export interface AppState {
-  whitelist: number[],
-  model: string,
-  openai: OpenAIApi,
+  whitelist: number[];
+  model: string;
+  openai: OpenAIApi;
 }
 
-export function run(cfg: AppConfig) {
+function envGetOrAbort(key: string): string {
+  const val = Deno.env.get(key);
+
+  if (!val) {
+    console.error(`ENV: ${key} is not found`);
+    Deno.exit(1);
+  }
+
+  return val;
+}
+
+function envGetOpenAIModel(): string {
+  const model = Deno.env.get("OPENAI_MODEL") || "gpt-3.5-turbo";
+  if (!["gpt-3.5-turbo", "text-davinci-003"].includes(model)) {
+    console.error(`Invalid model selection "${model}"`);
+    Deno.exit(1);
+  }
+
+  return model;
+}
+
+export function run() {
   const config = new Configuration({
-    apiKey: cfg.openai_token,
+    apiKey: envGetOrAbort("OPENAI_API_TOKEN"),
   });
 
   const state: AppState = {
     openai: new OpenAIApi(config),
-    ...cfg
+    whitelist: envGetOrAbort("WHITELIST_CHAT_IDS").split(",").map((elem) =>
+      parseInt(elem)
+    ),
+    model: envGetOpenAIModel(),
   };
 
-  dispatch(cfg.bot_token, state);
+  Bot.dispatch(envGetOrAbort("TGBOT_API_TOKEN"), state);
 }
